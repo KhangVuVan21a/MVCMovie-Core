69f6fa9ae67d25bca6def2e6e6841db7062c5366
KhangVuVan21a <khang21a@gmail.com>
2022-07-12 18:52:36 +0700

    fisrt commit

---
+++ MvcMovie-Core/Data/MvcMovie_CoreContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie_Core.Models;
+
+namespace MvcMovie_Core.Data
+{
+    public class MvcMovie_CoreContext : DbContext
+    {
+        public MvcMovie_CoreContext (DbContextOptions<MvcMovie_CoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MvcMovie_Core.Models.Movie>? Movie { get; set; }
+
+    }
+}
